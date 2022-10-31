350e8c69da003641366e9da7146bf0769a6ae6b5
itkhan <itkhanz@gmail.com>
2022-10-31 23:02:02 +0100

    headless browser driver

--- SeleniumFramework-RSA/src/test/java/itkhan/TestComponents/BaseTest.java
+++ SeleniumFramework-RSA/src/test/java/itkhan/TestComponents/BaseTest.java
@@ -5,10 +5,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import itkhan.pageobjects.LandingPage;
 import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
@@ -39,9 +41,14 @@ public class BaseTest {
         //To run tests on different browsers just run mvn test -Dbrowser=firefox
         String browserName = System.getProperty("browser")!=null ? System.getProperty("browser") :prop.getProperty("browser");
 
-        if (browserName.equalsIgnoreCase("chrome")) {
+        if (browserName.contains("chrome")) {
+            ChromeOptions options = new ChromeOptions();
             WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
+            if(browserName.contains("headless")){
+                options.addArguments("headless");
+            }
+            driver = new ChromeDriver(options);
+            driver.manage().window().setSize(new Dimension(1440,900));//full screen
         } else if (browserName.equalsIgnoreCase("firefox")) {
             WebDriverManager.firefoxdriver().setup();
             driver = new FirefoxDriver();
